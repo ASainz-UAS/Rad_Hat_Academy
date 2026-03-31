5c81753808abe2a3cd0750c615083aa321a33bd8
ASainz-UAS <ad.garcia25@info.uas.edu.mx>
2026-03-31 00:04:55 -0700

    Final-Version

--- E5_Area_Circulo.py
+++ E5_Area_Circulo.py
@@ -1,6 +1,6 @@
 area=0; radio=0;
-print("Algoritmo Que Prosesa El Area De Un Circulo/n")
-print("Uso De Decimales y Unidades De Medidas En Centimetros/n")
+print("Algoritmo Que Prosesa El Area De Un Circulo")
+print("Uso De Decimales y Unidades De Medidas En Centimetros")
 radio=float(input("Escriba El Radio Del Circulo:"))
-area=3.14159*radio
+area=3.1416*radio
 print(f"El Area Del Circulo Es: {area}")
