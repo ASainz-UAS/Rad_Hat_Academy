79f158d2d34b40e3f4589a93e0f789daa6bff813
ASainz-UAS <ad.garcia25@info.uas.edu.mx>
2026-04-01 19:23:37 -0700

    Final_Verson

---
+++ E1_String_Caracteres.py
@@ -0,0 +1,5 @@
+
+print("Este Programa Pide Un Texto y Imprime El Total De Caracteres Ingresados.")
+Lin1=input("Ingrese Un Texto: ")
+TotalC=len(Lin1.replace(" ",""))
+print(f"El Texto Ingresado Fue: {Lin1}. Con Un Total De {TotalC} Caracteres.")
