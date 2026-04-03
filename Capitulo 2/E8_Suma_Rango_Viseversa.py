76e29523671bc671047f491c937e3ea81c9afadb
ASainz-UAS <ad.garcia25@info.uas.edu.mx>
2026-04-03 03:38:53 -0700

    Final_Version

--- Capitulo 2/E8_Suma_Rango_Viseversa.py
+++ Capitulo 2/E8_Suma_Rango_Viseversa.py
@@ -2,6 +2,11 @@ print("Este Programa Solicita Numeros Enteros y Elabora La Suma Entre Todos Los 
 Num1=int(input("Introduzca El Primer Numero: "))
 Num2=int(input("Introduzca El Segundo Numero: "))
 suma=0
-for i in range (Num1,Num2+1):
-    suma=suma+i
+if Num1>Num2:
+    for i in range(Num2,Num1+1):
+        suma=suma+i
+else:
+    for i in range(Num1,Num2+1):
+        suma=suma+i
+
 print(f"La Suma Final Es: {suma}.")
