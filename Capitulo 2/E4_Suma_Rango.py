c479ae4edac185a28c114cee105ec0435a3e5ec2
ASainz-UAS <ad.garcia25@info.uas.edu.mx>
2026-04-02 21:28:01 -0700

    Final_Version

---
+++ Capitulo 2/E4_Suma_Rango.py
@@ -0,0 +1,11 @@
+print("Este Programa Solicita Numeros Enteros y Elabora La Suma Entre Todos Los Numeros Dentro De Ellos.")
+Num1=int(input("Introduzca El Primer Numero: "))
+Num2=int(input("Introduzca El Segundo Numero: "))
+suma=0
+for i in range (Num1,Num2+1):
+    suma=suma+i
+print(f"La Suma Final Es: {suma}.")
+
+
+
+
