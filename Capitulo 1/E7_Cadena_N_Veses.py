5559a749cdf198646eeef6126251542af77b5cbf
ASainz-UAS <ad.garcia25@info.uas.edu.mx>
2026-04-03 04:36:03 -0700

    Final_Version

---
+++ Capitulo 1/E7_Cadena_N_Veses.py
@@ -0,0 +1,5 @@
+print("Este Programa Solicita Una Cadena Y Un Numero Y Genera Una Nueva Cadena Repitiendo El Texto Segun La Cantidad Indicada.")
+Palabra=input("Introduzca Una Palabra: ")
+Numero=(int(input("Introduzca El Nimero De Repeticiones: ")))
+NPalabras=Palabra*Numero
+print(NPalabras)
