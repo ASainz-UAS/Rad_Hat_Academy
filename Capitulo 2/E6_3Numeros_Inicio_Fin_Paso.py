06f60660efdf9c947bc84183ba9ef208308e3a8a
ASainz-UAS <ad.garcia25@info.uas.edu.mx>
2026-04-03 02:50:40 -0700

    Final_Version

---
+++ Capitulo 2/E6_3Numeros_Inicio_Fin_Paso.py
@@ -0,0 +1,10 @@
+print("Este programa pide tres números (inicio, fin y paso) y muestra los valores en ese rango según el incremento indicado.")
+Inicio=int(input("Ingrese El Inicio Del Algoritmo: "))
+Fin=int(input("Ingrese El Fin Del Algoritmo: "))
+Paso=int(input("Ingrese Con Que Paso Ira El Algoritmo: "))
+for i in range (Inicio,Fin,Paso):
+    print(i)
+
+
+
+
