821f99c4dc8f410704e35c973441700f6d80413c
ASainz-UAS <ad.garcia25@info.uas.edu.mx>
2026-04-02 20:24:24 -0700

    Final_Version

---
+++ Capitulo 2/E3_Mayor_Igual_Menor.py
@@ -0,0 +1,18 @@
+print("Este Programa Solicita 2 Numeros Enteros y Valora Si Son Iguales O Quien Es Mayor.")
+Num1=input("Introduzca El Primer Numero: ")
+Num2=input("Introduzca El Segundo Numero: ")
+if Num1>Num2:
+    print(f"El {Num1} Es Mayor Que {Num2}.")
+elif Num1==Num2:
+    print(f"Los Numeros {Num1} y {Num2} Son Iguales.") 
+else:
+    print(f"El {Num2} Es Mayor Que {Num1}.")
+
+
+
+
+
+
+
+
+
