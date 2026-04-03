9d20685c34c282920096ccdca89bf2a103b25715
ASainz-UAS <ad.garcia25@info.uas.edu.mx>
2026-04-02 19:59:32 -0700

    Final_Version

---
+++ Capitulo 2/E1_Numeros_Suerte.py
@@ -0,0 +1,8 @@
+print("Este Programa Solicita Un Numero De La Suerte y Imprime Si Es Entero")
+Num_S=input("Ingrese Un Numero De La Suerte: ")
+if Num_S.isdigit():
+    print(f"El Numero Ingresado: {Num_S}, Es Un Entero.")
+else:
+    print(f"El Numero Ingresado: {Num_S}, No Es Un Entero.")
+
+
