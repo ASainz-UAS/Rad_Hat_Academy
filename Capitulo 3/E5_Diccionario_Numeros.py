899c948884235364691e8d1058100a74da61374d
ASainz-UAS <ad.garcia25@info.uas.edu.mx>
2026-04-06 22:28:36 -0700

    Final_Version

---
+++ Capitulo 3/E5_Diccionario_Numeros.py
@@ -0,0 +1,5 @@
+print("Este Programa Solicita Un Número Al Usuario Y Convierte Cada Dígito En Su Representación En Palabras En Español Utilizando Un Diccionario.")
+Numeros={"0":"Zero","1":"Uno","2":"Dos","3":"Tres","4":"Cuatro","5":"Cinco","6":"Seis","7":"Siete","8":"Ocho","9":"Nueve"}
+Num=input("Ingrese Un Numero: ")
+for i in Num:
+    print(f"{Numeros[i]}", end=" ")
