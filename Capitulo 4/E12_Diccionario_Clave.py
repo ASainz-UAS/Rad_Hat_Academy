4fab6f8481a1dd2c58ffa5148b810b4e6058ccec
ASainz-UAS <ad.garcia25@info.uas.edu.mx>
2026-04-10 21:46:22 -0700

    Final_Version

---
+++ Capitulo 4/E12_Diccionario_Clave.py
@@ -0,0 +1,17 @@
+#Introducción.
+print("Este Programa Define Una Funcion Que Recibe Un Numero Y Un Diccionario Como Parametros Y Agrega El Numero A Todos Los Valores Del Diccionario, Devolviendo El Diccionario Actualizado.")
+#Diccionario Principal:
+dic={"manzanas":10,"peras":5,"naranjas":8,"uvas":12,"mangos":6,"platanos":9}
+#Pide Un Valor Para Aumentar.
+Num=int(input("Ingrese El Numero De Aumento: "))
+#Función Principal:
+def aumento(valor,Diccionario):
+    #Aumenta Cada Valor.
+    for i in Diccionario:
+        Diccionario[i]=Diccionario[i]+valor
+    return Diccionario
+#Llama A La Función.
+Dic1=aumento(Num,dic)
+#imprime El Resultado Del Algoritmo.
+print(f"El Diccionario Con Su Aumento Queda Asi: {Dic1}.")
+
