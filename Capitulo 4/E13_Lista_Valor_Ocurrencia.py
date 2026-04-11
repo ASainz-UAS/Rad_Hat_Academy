9de947408982db09df1f9f3544b77aaa62beffbf
ASainz-UAS <ad.garcia25@info.uas.edu.mx>
2026-04-10 22:26:24 -0700

    Final_Version

--- Capitulo 4/E13_Lista_Valor_Ocurrencia.py
+++ Capitulo 4/E13_Lista_Valor_Ocurrencia.py
@@ -2,17 +2,21 @@
 print("Este Programa Define Una Funcion Que Busca Un Elemento En Una Lista Y Devuelve El Indice Correspondiente A La Primera, Segunda O Tercera Ocurrencia Segun Se Indique.")
 #Captura Una Lista:
 Lis=input("Escriba Los Numeros Que Desea Ingresar A La Primera Lista: ")
+#Convierte Cada Valor A Int.
 Lista = [int(i) for i in Lis.split()]
 Valor=int(input("¿Que Valor Desea Buscar? "))
 lugar=int(input("¿En Que N-Veses Desea Buscar El Valor? "))
+#Función Principal:
 def buscar(l,v,x):
     contador=0
+    #Busca i Dentro Del Rango De La Lista.
     for i in range(len(l)):
         if l[i]==v:
             contador+=1
             if contador==x:
                 return i
     raise ValueError("No se encontró la ocurrencia")
+#Ejecuta La Función.
 resultado=buscar(Lista, Valor, lugar)
+#Imprime El Resultado Del Algoritmo.
 print(f"El Valor {Valor} Aparece En El Indice {resultado}.")
-
