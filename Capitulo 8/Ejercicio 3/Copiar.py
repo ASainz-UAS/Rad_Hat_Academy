4d467040ea840f349667fe4068aa595d1a62a9b6
ASainz-UAS <ad.garcia25@info.uas.edu.mx>
2026-06-03 18:04:01 -0700

    Final_Version

---
+++ Capitulo 8/Ejercicio 3/Copiar.py
@@ -0,0 +1,14 @@
+#Introduccion.
+print("Este programa copia el contenido de un archivo de texto a otro archivo, leyendo cada línea con readline() y escribiéndola en un nuevo archivo.")
+#Pide El Nombre Del Archivo.
+Entrada=input("Como Se Llama El Archivo De Entrada: ")
+Salida=input("Como Se Llamara El Archivo De Entrada: ")
+#Abre Los Archivos
+Archivo=open(Entrada, "r")
+ArchivoNuevo=open(Salida, "w")
+#Copia Cada Linea De Archivo De Entrada Al De Salida.
+Lineas=Archivo.readline()
+while Lineas!="":
+    ArchivoNuevo.write(Lineas)
+    Lineas=Archivo.readline()
+print("Archivo Copiado...")
