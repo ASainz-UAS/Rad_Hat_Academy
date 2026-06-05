3723a69fa373696cb5177c41c1655b269f7e3704
ASainz-UAS <ad.garcia25@info.uas.edu.mx>
2026-06-04 19:34:09 -0700

    Final_Version

--- Capitulo 8/Ejercicio 8/Contador_De_Repeticiones.py
+++ Capitulo 8/Ejercicio 8/Contador_De_Repeticiones.py
@@ -1,4 +1,19 @@
 #Introducción.
 print("Este Programa Lee Varios Archivos De Texto Indicados Desde La Linea De Comandos Y Cuenta Cuantas Veces Aparece Cada Nombre En Todos Los Archivos, Mostrando El Total De Apariciones De Cada Uno.")
-
-
+from Separador import SepararArchivo
+from Imprimir import imprimir
+import sys
+Archivos=sys.argv[1:]
+Conteo={}
+#Como No Se Sabe Cuantos Archivos Se Compararan Se Usa Un For.
+for Archivo in Archivos:
+    ArchivoN=open(Archivo, "r")
+    Nombres=SepararArchivo(ArchivoN)
+    #Valida Si Los Nombres Existen, y Si Existen, Incrementa Un Contador.
+    for Nombre in Nombres:
+        if Nombre in Conteo:
+            Conteo[Nombre]=Conteo[Nombre]+1
+        else:
+            Conteo[Nombre]=1
+#Imprime Los Resutados.
+imprimir(Conteo)
--- Capitulo 8/Ejercicio 8/Imprimir.py
+++ Capitulo 8/Ejercicio 8/Imprimir.py
@@ -1,16 +1,7 @@
-def imprimir (NombresRepetidos, Nombres1, Nombres2, Archivos):
+def imprimir (Conteo):
     i=1
-    print("\nContenido De:", Archivos[0])
-    for Nombre in Nombres1:
+    print("\nLista Final: ")
+    for Nombre, Cantidad in Conteo.items():
         print(f"{i}º {Nombre}")
-        i=i+1
-    i=1
-    print("\nContenido De:", Archivos[1])
-    for Nombre in Nombres2:
-        print(f"{i}º {Nombre}")
-        i=i+1
-    i=1
-    print("\nRepetidos: ")
-    for Nombre in NombresRepetidos:
-        print(f"{i}º {Nombre}")
+        print(f"Cantidad: {Cantidad}.")
         i=i+1
